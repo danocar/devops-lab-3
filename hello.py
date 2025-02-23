13cb29e850efcc41d7df0dd30bf7d20001225f79
Daniel <daniel@MACBOOKAIR-67F0.station>
2025-02-23 21:12:17 +0000

    Added a route on the hello world page[C

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello, World!</p>'
+    return '<p>Hello, World!</p><p><a href="/about">Go to About Page</a></p>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
